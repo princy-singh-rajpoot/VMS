5b31ebd8acb408c31c482a25933528aba41a1f80
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-05-20 17:43:22 +0530

    commiting the get/api/vendor/vendor_id

--- Vendor/urls.py
+++ Vendor/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from .views import create_vendor,list_vendor
+from .views import create_vendor,list_vendor,specific_vendor
+from . import views
 
 urlpatterns = [
     path('vendor/', create_vendor),
     path('vendorslist/', list_vendor),
+    path('vendor/<int:vendor_id>/', specific_vendor),
 ]
--- Vendor/views.py
+++ Vendor/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from .models import Vendor,PurchaseOrder,HistoricalPerformance
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from django.http import JsonResponse
+from django.http import JsonResponse,Http404
+
+def specific_vendor(request,vendor_id):
+   try:
+        vendor = Vendor.objects.get(id=vendor_id)
+        vendor_data = {
+            'id': vendor.id,
+            'name': vendor.name,
+            'contact_details': vendor.contact_details,
+            'address': vendor.address,
+            'vendor_code':vendor.vendor_code,
+            'on_time_delivery_rate': vendor.on_time_delivery_rate,
+            'quality_rating_avg': vendor.quality_rating_avg,
+            'fulfillment_rate': vendor.fulfillment_rate,
+        }
+        return JsonResponse(vendor_data)
+   except Vendor.DoesNotExist:
+    raise Http404("Vendor does not exist")
 
 def list_vendor(request):
     vendors = Vendor.objects.all().values()
@@ -13,6 +30,7 @@ def list_vendor(request):
 @permission_classes([AllowAny])
 def create_vendor(request):
     name = request.data['name']
+    contact_details = request.data['contact_details']
     vendor_code = request.data['vendor_code']
     on_time_delivery_rate = request.data['on_time_delivery_rate']
     quality_rating_avg = request.data['quality_rating_avg']
@@ -21,6 +39,7 @@ def create_vendor(request):
     
     new_vendor = Vendor(
         name=name,
+        contact_details=contact_details,
         vendor_code=vendor_code,
         on_time_delivery_rate=on_time_delivery_rate,
         quality_rating_avg=quality_rating_avg,
